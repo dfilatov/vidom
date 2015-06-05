d30cf973eb737b7fe2ec2cb5c2381c7faa733183
Dmitry Filatov <dfilatov@yandex-team.ru>
2015-06-05 23:44:24 +0300

    add specs for "removeAttr" op

--- spec/patchDom/patchDom.spec.js
+++ spec/patchDom/patchDom.spec.js
@@ -13,7 +13,7 @@ describe('patchDom', function() {
 
     describe('updateAttr', function() {
         it('should update node attribute', function() {
-            var domNode = renderToDom({ tag : 'textarea', attrs : { cols : 5, rows : 2, disabled : true } });
+            var domNode = renderToDom({ tag : 'textarea', attrs : { cols : 5 } });
             patchDom(domNode, [{ type : patchOps.UPDATE_ATTR, path : '', attrName : 'cols', attrVal : 3 }]);
             expect(domNode.getAttribute('cols')).to.equal('3');
         });
@@ -22,6 +22,20 @@ describe('patchDom', function() {
             var domNode = renderToDom({ tag : 'input', attrs : { value : 'val' } });
             patchDom(domNode, [{ type : patchOps.UPDATE_ATTR, path : '', attrName : 'value', attrVal : 'new val' }]);
             expect(domNode.value).to.equal('new val');
+        });
+    });
+
+    describe('removeAttr', function() {
+        it('should remove node attribute', function() {
+            var domNode = renderToDom({ tag : 'textarea', attrs : { disabled : true } });
+            patchDom(domNode, [{ type : patchOps.REMOVE_ATTR, path : '', attrName : 'disabled' }]);
+            expect(domNode.hasAttribute('disabled')).to.equal(false);
+        });
+
+        it('should remove node property', function() {
+            var domNode = renderToDom({ tag : 'input', attrs : { value : 'val' } });
+            patchDom(domNode, [{ type : patchOps.REMOVE_ATTR, path : '', attrName : 'value' }]);
+            expect(domNode.value).to.equal('');
         });
     });
 });
