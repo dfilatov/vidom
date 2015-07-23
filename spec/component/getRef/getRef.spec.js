69202c9c0d331b5f4b41b9893969bdf1d88781fb
dfilatov <dfilatov@yandex-team.ru>
2015-07-23 19:22:37 +0300

    fix reset refs on each rerender

--- spec/component/getRef/getRef.spec.js
+++ spec/component/getRef/getRef.spec.js
@@ -13,7 +13,7 @@ describe('getRef', function() {
         document.body.removeChild(domNode);
     });
 
-    it('should point to right DOM element', function(done) {
+    it('should point to the right DOM element', function(done) {
         var C = createComponent({
                 onRender : function() {
                     return createNode('div').children([
@@ -28,6 +28,38 @@ describe('getRef', function() {
                 }
             });
 
+        mounter.mountToDomSync(domNode, createNode(C));
+    });
+
+    it('should be reset on each render', function(done) {
+        var switchRef = true,
+            C = createComponent({
+                onRender : function() {
+                    var refNode = createNode('span');
+
+                    if(switchRef) {
+                        refNode
+                            .attrs({ id : 'id1' })
+                            .ref('control');
+                        switchRef = false;
+                    }
+                    else {
+                        refNode
+                            .attrs({ id : 'id2' })
+                            .ref('control-new');
+                    }
+
+                    return createNode('div').children([createNode('div'), refNode]);
+                },
+
+                onUpdate : function() {
+                    expect(this.getRef('control')).to.be.equal(null);
+                    expect(this.getRef('control-new')).to.be.equal(document.getElementById('id2'));
+                    done();
+                }
+            });
+
+        mounter.mountToDomSync(domNode, createNode(C));
         mounter.mountToDomSync(domNode, createNode(C));
     });
 });
