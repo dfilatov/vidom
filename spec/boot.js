51da94808374e38daf62d7e680a8e9e8f6442b18
dfilatov <dfilatov@yandex-team.ru>
2015-08-10 13:08:03 +0300

    Implement SSR

--- spec/boot.js
+++ spec/boot.js
@@ -1,5 +1,6 @@
 var expect = require('expect.js');
 require('./renderToDom/renderToDom.spec.js');
+require('./renderToString/renderToString.spec.js');
 require('./patch/patch.spec.js');
 require('./patchDom/patchDom.spec.js');
 require('./mountToDom/mountToDom.spec.js');
