9eb2ae4b4342e341c3d73163fce39edbf6a28d62
dfilatov <dfilatov@yandex-team.ru>
2015-08-20 18:48:14 +0300

    get rid of usage of "npm" module

--- scripts/release.js
+++ scripts/release.js
@@ -1,27 +1,26 @@
-var npm = require('npm'),
-    vow = require('vow'),
+var vow = require('vow'),
     vowNode = require('vow-node'),
     childProcess = require('child_process'),
     version = process.argv.slice(2)[0] || 'patch';
 
-vowNode.invoke(childProcess.exec, 'git pull')
+function execCommand(command) {
+    vowNode.invoke(childProcess.exec, command);
+}
+
+execCommand('git pull')
     .then(function() {
-        return vowNode.invoke(npm.load);
-    })
-    .then(function(npm) {
         return vow.all([
-            vowNode.invoke(npm.commands.runScript, ['build-lib']),
-            vowNode.invoke(npm.commands.runScript, ['build-dist'])
-        ]).then(function() {
-            return vowNode.invoke(npm.commands.version, [version]);
-        }).then(function() {
-            return vowNode.invoke(childProcess.exec, 'git push --follow-tags');
-        })
-        .then(function() {
-            return vowNode.invoke(npm.load);
-        })
-        .then(function(npm) {
-            return vowNode.invoke(npm.commands.publish);
-        });
+            execCommand('npm run-script build-lib'),
+            execCommand('npm run-script build-dist')
+        ]);
+    })
+    .then(function() {
+        return execCommand('npm version ' + version);
+    })
+    .then(function() {
+        return execCommand('git push --follow-tags');
+    })
+    .then(function() {
+        return execCommand('npm publish');
     })
     .done();
