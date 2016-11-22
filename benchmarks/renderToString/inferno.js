cb8129c047e07713c80d699f2279f652503ac3bd
Dmitry Filatov <dfilatov@yandex-team.ru>
2016-11-22 09:43:16 +0300

    Take inferno components back

--- benchmarks/renderToString/inferno.js
+++ benchmarks/renderToString/inferno.js
@@ -5,47 +5,53 @@ global.window = { screen : {} };
 
 const Inferno = require('inferno');
 const InfernoServer = require('inferno-server');
+const InfernoComponent = require('inferno-component');
 
 const createVNode = Inferno.createVNode;
 
-function App(props) {
-    const childrenNum = props.childrenNum;
+class App extends InfernoComponent {
+    render(props) {
+        const childrenNum = props.childrenNum;
 
-    return createVNode(66, 'div', { className : 'app' }, [
-        createVNode(8, Header, { childrenNum }, null, null, null, true),
-        createVNode(8, Content, { childrenNum }, null, null, null, true),
-        createVNode(8, Footer, { childrenNum }, null, null, null, true)
-    ], null, null, false);
-}
-
-function Header(props) {
-    const childrenNum = props.childrenNum,
-        children = [];
-    let i = 0;
-
-    while(i < childrenNum) {
-        children.push(createVNode(2, 'div', { id : 'header-' + i++ }, null, null, null, true));
+        return createVNode(66, 'div', { className : 'app' }, [
+            createVNode(4, Header, { childrenNum }, null, null, null, true),
+            createVNode(4, Content, { childrenNum }, null, null, null, true),
+            createVNode(4, Footer, { childrenNum }, null, null, null, true)
+        ], null, null, false);
     }
-
-    return createVNode(2, 'div', { className : 'header' }, children, null, null, true);
 }
 
-function Content(props) {
-    const childrenNum = props.childrenNum,
-        children = [];
-    let i = 0;
+class Header extends InfernoComponent {
+    render(props) {
+        const childrenNum = props.childrenNum,
+            children = [];
+        let i = 0;
 
-    while(i < childrenNum) {
-        children.push(
-            createVNode(2, 'b', null, 'bold' + i, null, null, null, true),
-            createVNode(2, 
-                'span',
-                { className : 'link' },
-                createVNode(8, Link, { href : '/', value : 'link-' + i }, null, null, null, true), null, null, true),
-            createVNode(2, 'i', null, 'italic' + i++, null, null, true),
-            createVNode(2, 
-                'div',
-                null,
+        while(i < childrenNum) {
+            children.push(createVNode(2, 'div', { id : 'header-' + i++ }, null, null, null, true));
+        }
+
+        return createVNode(2, 'div', { className : 'header' }, children, null, null, true);
+    }
+}
+
+class Content extends InfernoComponent {
+    render(props) {
+        const childrenNum = props.childrenNum,
+            children = [];
+        let i = 0;
+
+        while(i < childrenNum) {
+            children.push(
+                createVNode(2, 'b', null, 'bold' + i, null, null, null, true),
+                createVNode(2,
+                    'span',
+                    { className : 'link' },
+                    createVNode(4, Link, {
+                        href : '/',
+                        value : 'link-' + i
+                    }, null, null, null, true), null, null, true),
+                createVNode(2, 'i', null, 'italic' + i++, null, null, true),
                 createVNode(2,
                     'div',
                     null,
@@ -55,26 +61,34 @@ function Content(props) {
                         createVNode(2,
                             'div',
                             null,
-                            'div', null, null, true), null, null, true), null, null, true), null, null, true));
+                            createVNode(2,
+                                'div',
+                                null,
+                                'div', null, null, true), null, null, true), null, null, true), null, null, true));
+        }
+
+        return createVNode(66, 'div', { className : 'header' }, children, null, null, true);
     }
-
-    return createVNode(66, 'div', { className : 'header' }, children, null, null, true);
 }
 
-function Link(props) {
-    return createVNode(2, 'a', { href : props.href }, props.value, null, null, true);
-}
-
-function Footer(props) {
-    const childrenNum = props.childrenNum,
-        children = [];
-    let i = 0;
-
-    while(i < childrenNum) {
-        children.push(createVNode(2, 'div', { id : 'footer-' + i++ }, null, null, true));
+class Link extends InfernoComponent {
+    render(props) {
+        return createVNode(2, 'a', { href : props.href }, props.value, null, null, true);
     }
-
-    return createVNode(2, 'div', { className : 'footer' }, children, null, null, true);
 }
 
-module.exports = childrenNum => () => InfernoServer.renderToString(createVNode(8, App, { childrenNum }, null, null, null, true));
+class Footer extends InfernoComponent {
+    render(props) {
+        const childrenNum = props.childrenNum,
+            children = [];
+        let i = 0;
+
+        while(i < childrenNum) {
+            children.push(createVNode(2, 'div', { id : 'footer-' + i++ }, null, null, true));
+        }
+
+        return createVNode(2, 'div', { className : 'footer' }, children, null, null, true);
+    }
+}
+
+module.exports = childrenNum => () => InfernoServer.renderToString(createVNode(4, App, { childrenNum }, null, null, null, true));
