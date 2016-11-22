3d3b126ee780dd9dc94b6513f374d21ee612b0ee
Sultan Tarimo <sultantarimo@me.com>
2016-11-22 09:56:51 +0200

    Add Dio to SSR Benchmark (#259)
    Add dio
    
    * add dio
    
    * Create dio.js
    
    * cleanup module.exports for dio

--- benchmarks/renderToString/index.js
+++ benchmarks/renderToString/index.js
@@ -5,5 +5,6 @@ const CHILDREN_NUM = 200;
 module.exports = {
     'react' : require('./react')(CHILDREN_NUM),
     'inferno' : require('./inferno')(CHILDREN_NUM),
-    'vidom' : require('./vidom')(CHILDREN_NUM)
+    'vidom' : require('./vidom')(CHILDREN_NUM),
+    'dio.js': require('./dio.js')(CHILDREN_NUM)
 };
