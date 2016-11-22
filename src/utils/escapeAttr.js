c632cea9a7f44089ddd24e03cb27fe9c41eebee8
Dmitry Filatov <dfilatov@yandex-team.ru>
2016-11-22 11:19:42 +0300

    optimize escaping

--- src/utils/escapeAttr.js
+++ src/utils/escapeAttr.js
@@ -1,12 +1,32 @@
+const AMP_RE = /&/g,
+    QUOT_RE = /"/g;
+
 export default function escapeAttr(str) {
     str = str + '';
 
-    if(~str.indexOf('&')) {
-        str = str.replace(/&/g, '&amp;');
+    const len = str.length;
+    let i = 0,
+        escapeAmp = false,
+        escapeQuot = false;
+
+    while(i < len) {
+        switch(str[i++]) {
+            case '&':
+                escapeAmp = true;
+                break;
+
+            case '"':
+                escapeQuot = true;
+                break;
+        }
     }
 
-    if(~str.indexOf('"')) {
-        str = str.replace(/"/g, '&quot;');
+    if(escapeAmp) {
+        str = str.replace(AMP_RE, '&amp;');
+    }
+
+    if(escapeQuot) {
+        str = str.replace(QUOT_RE, '&quot;');
     }
 
     return str;
--- src/utils/escapeHtml.js
+++ src/utils/escapeHtml.js
@@ -1,16 +1,42 @@
+const AMP_RE = /&/g,
+    LT_RE = /</g,
+    GT_RE = />/g;
+
 export default function escapeHtml(str) {
     str = str + '';
 
-    if(~str.indexOf('&')) {
-        str = str.replace(/&/g, '&amp;');
+    const len = str.length;
+    let i = 0,
+        escapeAmp = false,
+        escapeLt = false,
+        escapeGt = false;
+
+    while(i < len) {
+        switch(str[i++]) {
+            case '&':
+                escapeAmp = true;
+                break;
+
+            case '<':
+                escapeLt = true;
+                break;
+
+            case '>':
+                escapeGt = true;
+                break;
+        }
     }
 
-    if(~str.indexOf('<')) {
-        str = str.replace(/</g, '&lt;');
+    if(escapeAmp) {
+        str = str.replace(AMP_RE, '&amp;');
     }
 
-    if(~str.indexOf('>')) {
-        str = str.replace(/>/g, '&gt;');
+    if(escapeLt) {
+        str = str.replace(LT_RE, '&lt;');
+    }
+
+    if(escapeGt) {
+        str = str.replace(GT_RE, '&gt;');
     }
 
     return str;
