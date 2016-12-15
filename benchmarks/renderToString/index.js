8325cbafd10ef5a0488123272496e15812d64a7c
Dmitry Filatov <dfilatov@yandex-team.ru>
2016-12-15 14:46:06 +0300

    add preact

--- benchmarks/renderToString/index.js
+++ benchmarks/renderToString/index.js
@@ -4,6 +4,7 @@ const CHILDREN_NUM = 200;
 
 module.exports = {
     'react' : require('./react')(CHILDREN_NUM),
+    'preact' : require('./preact')(CHILDREN_NUM),
     'inferno' : require('./inferno')(CHILDREN_NUM),
     'vidom' : require('./vidom')(CHILDREN_NUM),
     'dio.js': require('./dio.js')(CHILDREN_NUM)
