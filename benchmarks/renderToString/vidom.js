e573b0b064d101bf0475a8acca156738a7f2ccd4
Dmitry Filatov <dfilatov@yandex-team.ru>
2017-02-09 23:20:10 +0300

    up vidom and preact

--- benchmarks/renderToString/vidom.js
+++ benchmarks/renderToString/vidom.js
@@ -4,72 +4,78 @@ const vidom = require('vidom'),
     node = vidom.node;
 
 class App extends vidom.Component {
-    onRender(attrs) {
-        const childrenNum = attrs.childrenNum;
+    onRender() {
+        const childrenNum = this.attrs.childrenNum;
 
-        return node('div').attrs({ className : 'app' }).children([
-            node(Header).attrs({ childrenNum }),
-            node(Content).attrs({ childrenNum }),
-            node(Footer).attrs({ childrenNum })
+        return node('div').setAttrs({ className : 'app' }).setChildren([
+            node(Header).setAttrs({ childrenNum }),
+            node(Content).setAttrs({ childrenNum }),
+            node(Footer).setAttrs({ childrenNum })
         ]);
     }
 }
 
 class Header extends vidom.Component {
-    onRender(attrs) {
-        const childrenNum = attrs.childrenNum,
+    onRender() {
+        const childrenNum = this.attrs.childrenNum,
             children = [];
         let i = 0;
 
         while(i < childrenNum) {
-            children.push(node('div').attrs({ id : 'header-' + i++ }));
+            children.push(node('div').setAttrs({ id : 'header-' + i++ }));
         }
 
-        return node('div').attrs({ className : 'header' }).children(children);
+        return node('div').setAttrs({ className : 'header' }).setChildren(children);
     }
 }
 
 class Content extends vidom.Component {
-    onRender(attrs) {
-        const childrenNum = attrs.childrenNum,
+    onRender() {
+        const childrenNum = this.attrs.childrenNum,
             children = [];
         let i = 0;
 
         while(i < childrenNum) {
             children.push(
-                node('b').children('bold' + i),
+                node('b').setChildren('bold' + i),
                 node('span')
-                    .attrs({ className : 'link' })
-                    .children(node(Link).attrs({ href : '/', value : 'link-' + i })),
-                node('i').children('italic' + i++),
-                node('div').children(
-                    node('div').children(
-                        node('div').children(
-                            node('div').children('div')))));
+                    .setAttrs({ className : 'link' })
+                    .setChildren(node(Link).setAttrs({ href : '/', value : 'link-' + i })),
+                node('i').setChildren('italic' + i++),
+                node('div').setChildren(
+                    node('div').setChildren(
+                        node('div').setChildren(
+                            node('div').setChildren('div')))));
         }
 
-        return node('div').attrs({ className : 'content' }).children(children);
+        return node('div')
+            .setAttrs({ className : 'content' })
+            .setChildren(children);
     }
 }
 
 class Link extends vidom.Component {
-    onRender(attrs) {
-        return node('a').attrs({ href : attrs.href }).children(attrs.value);
+    onRender() {
+        return node('a')
+            .setAttrs({ href : this.attrs.href })
+            .setChildren(this.attrs.value);
     }
 }
 
 class Footer extends vidom.Component {
-    onRender(attrs) {
-        const childrenNum = attrs.childrenNum,
+    onRender() {
+        const childrenNum = this.attrs.childrenNum,
             children = [];
         let i = 0;
 
         while(i < childrenNum) {
-            children.push(node('div').attrs({ id : 'footer-' + i++ }));
+            children.push(node('div').setAttrs({ id : 'footer-' + i++ }));
         }
 
-        return node('div').attrs({ className : 'footer' }).children(children);
+        return node('div')
+            .setAttrs({ className : 'footer' })
+            .setChildren(children);
     }
 }
 
-module.exports = childrenNum => () => vidom.renderToString(node(App).attrs({ childrenNum }));
+module.exports = childrenNum => () => vidom.renderToString(node(App).setAttrs({ childrenNum }));
