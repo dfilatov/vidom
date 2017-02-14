8ab59e67cae4b290757526d1a9a74dc1cc774d10
Dmitry Filatov <dfilatov@yandex-team.ru>
2017-02-14 22:40:57 +0300

    mount: throw an error in case of passing a top node with wrong type in dev mode (closes #291)

--- src/nodes/utils/isNode.js
+++ src/nodes/utils/isNode.js
@@ -1,8 +1,8 @@
-import ComponentNode from '../ComponentNode';
 import FragmentNode from '../FragmentNode';
 import FunctionComponentNode from '../FunctionComponentNode';
 import TagNode from '../TagNode';
 import TextNode from '../TextNode';
+import ComponentNode from '../ComponentNode';
 
 export default function isNode(obj) {
     return obj instanceof ComponentNode ||
