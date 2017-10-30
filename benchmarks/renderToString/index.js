3db6c91264f208b7e5e9c9a5683f4906b3e4f261
Dmitry Filatov <dfilatov@yandex-team.ru>
2017-10-30 17:56:38 +0300

    up all

--- benchmarks/renderToString/index.js
+++ benchmarks/renderToString/index.js
@@ -4,7 +4,6 @@ const CHILDREN_NUM = 200;
 
 module.exports = {
     'react' : require('./react')(CHILDREN_NUM),
-    'react.with-hack' : require('./react.with-hack')(CHILDREN_NUM),
     'preact' : require('./preact')(CHILDREN_NUM),
     'inferno' : require('./inferno')(CHILDREN_NUM),
     'vidom' : require('./vidom')(CHILDREN_NUM),
