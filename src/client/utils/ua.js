1a48a1b83fb10aa44e9bb02f89eedc0fd93588cb
Dmitry Filatov <dfilatov@yandex-team.ru>
2018-08-13 10:54:35 +0300

    Use navigator.platform to detect ios in desktop mode

--- src/client/utils/ua.js
+++ src/client/utils/ua.js
@@ -1,5 +1,6 @@
-const ua = typeof navigator === 'undefined'? '' : navigator.userAgent;
+const ua = typeof navigator === 'undefined'? '' : navigator.userAgent,
+    platform = typeof navigator === 'undefined'? '' : navigator.platform;
 
 export const isTrident = ua.indexOf('Trident') > -1;
 export const isEdge = ua.indexOf('Edge') > -1;
-export const isIos = /iPad|iPhone|iPod/.test(ua) && typeof MSStream === 'undefined';
+export const isIos = /iPad|iPhone|iPod/.test(platform);
